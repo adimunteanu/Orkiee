951ce2811c1634a2816add913505543159ae828c
Gabi <gabimunteanu.sdt@gmail.com>
2017-01-19 08:23:30 +0100

    new project

--- Orkiee/Scripts/main.js
+++ Orkiee/Scripts/main.js
@@ -1,15 +1,17 @@
-﻿$(document).ready(function(){
+﻿project = function (data) {
+    this.name = ko.observable(data.name);
+    this.procent = ko.observable(data.procent);
+}
+
+$(document).ready(function () {
     function AppViewModel() {
         var self = this;
 
         self.projects = ko.observableArray();
 
         self.addProject = function () {
-            var object = {
-                name: 'Project',
-                procent:'0%'
-            }
-            self.projects.push(object);
+            var new_project = new project({name: "Project", procent: 0});
+            self.projects.push(new_project);
         };
 
 
