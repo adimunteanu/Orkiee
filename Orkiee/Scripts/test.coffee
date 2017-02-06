45b69f54e5e01cdc7e44b4c961dc8f40d0d49277
adimunteanu <adimunteanu13@gmail.com>
2017-02-06 18:14:13 +0200

    Am scris exemplul array din knockoutjs in coffeescript

--- Orkiee/Scripts/test.coffee
+++ Orkiee/Scripts/test.coffee
@@ -1,56 +1,16 @@
 ﻿# CoffeeScript
-class project
-    constructor: (data) ->
-        @id = ko.observable data.id 
-        @name = ko.observable data.name || 'blank'
-        @percent = ko.observable data.percent || '0%'
-        @project_class = ko.observable "slider_" + @id().toString()
-        @createSlider = =>
-            slider_properties={
-                range : "min"
-                min: 0
-                max: 100
-                value: 0
-                slide: (event,ui) =>
-                    @percent(ui.value)
-            }
-            div = $(".slider_" + @id())
-            console.log(div)
-            div.slider(slider_properties)
- 
-class projectsView
-    constructor: ->
-        slider_properties={
-            range : "min"
-            min: 0
-            max: 100
-            value: 0
-            slide: (event,ui)->
-                $(".amount").val(ui.value)
-        }
-        $(".slider").slider(slider_properties)
-        $(".amount").val( $(".slider").slider("value"))
-        @projects = ko.observableArray []
-        @new_title = ko.observable ''
-        @addProject = =>
-            data = {
-                id: @projects.length
-                name: @new_title(),
-                percent: '0%'
-            }
-            new_project = new project(data)
-            @projects.push(new_project)
-            setTimeout(new_project.createSlider, 200)
-        @username = ko.observable ''
-        @password = ko.observable ''
-
-    
-window.myview  = => new projectsView
-
-window.onload = ->
-    window.my_view = new myview()
-    ko.applyBindings(my_view)
-
-
-    
-        
+SimpleListModel=(items)->
+    @items=ko.observableArray(items)
+    @itemToAdd=ko.observable ('')
+    @addItem=(->
+        if @itemsToAdd!=""
+            @items.push @itemToAdd()
+            @itemToAdd ""
+            return
+    ).bind(this)
+    return
+ko.applyBindings new SimpleListModel([
+    "Alpha"
+    "Beta"
+    "Gamma"
+])
--- Orkiee/Scripts/test.js
+++ Orkiee/Scripts/test.js
@@ -1,82 +1,18 @@
 ﻿(function() {
-  var project, projectsView;
+  var SimpleListModel;
 
-  project = (function() {
-    function project(data) {
-      this.id = ko.observable(data.id);
-      this.name = ko.observable(data.name || 'blank');
-      this.percent = ko.observable(data.percent || '0%');
-      this.project_class = ko.observable("slider_" + this.id().toString());
-      this.createSlider = (function(_this) {
-        return function() {
-          var div, slider_properties;
-          slider_properties = {
-            range: "min",
-            min: 0,
-            max: 100,
-            value: 0,
-            slide: function(event, ui) {
-              return _this.percent(ui.value);
-            }
-          };
-          div = $(".slider_" + _this.id());
-          console.log(div);
-          return div.slider(slider_properties);
-        };
-      })(this);
-    }
-
-    return project;
-
-  })();
-
-  projectsView = (function() {
-    function projectsView() {
-      var slider_properties;
-      slider_properties = {
-        range: "min",
-        min: 0,
-        max: 100,
-        value: 0,
-        slide: function(event, ui) {
-          return $(".amount").val(ui.value);
-        }
-      };
-      $(".slider").slider(slider_properties);
-      $(".amount").val($(".slider").slider("value"));
-      this.projects = ko.observableArray([]);
-      this.new_title = ko.observable('');
-      this.addProject = (function(_this) {
-        return function() {
-          var data, new_project;
-          data = {
-            id: _this.projects.length,
-            name: _this.new_title(),
-            percent: '0%'
-          };
-          new_project = new project(data);
-          _this.projects.push(new_project);
-          return setTimeout(new_project.createSlider, 200);
-        };
-      })(this);
-      this.username = ko.observable('');
-      this.password = ko.observable('');
-    }
-
-    return projectsView;
-
-  })();
-
-  window.myview = (function(_this) {
-    return function() {
-      return new projectsView;
-    };
-  })(this);
-
-  window.onload = function() {
-    window.my_view = new myview();
-    return ko.applyBindings(my_view);
+  SimpleListModel = function(items) {
+    this.items = ko.observableArray(items);
+    this.itemToAdd = ko.observable('');
+    this.addItem = (function() {
+      if (this.itemsToAdd !== "") {
+        this.items.push(this.itemToAdd());
+        this.itemToAdd("");
+      }
+    }).bind(this);
   };
+
+  ko.applyBindings(new SimpleListModel(["Alpha", "Beta", "Gamma"]));
 
 }).call(this);
 
